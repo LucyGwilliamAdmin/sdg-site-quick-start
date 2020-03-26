f755740e083a5dedfeb26b6c7cf44c96912ccc0c
Lucy <43404358+LucyGwilliamAdmin@users.noreply.github.com>
2020-03-26 17:14:27 +0000

    Add files via upload

--- scripts/batch/add_language.py
+++ scripts/batch/add_language.py
@@ -19,9 +19,9 @@ import glob
 import sys
 import os
 
-def add_language(new_language, language_public):
-    # We only care about one folder.
-    folders = ['_pages']
+def add_language(new_language):
+    # We only care about these three folders.
+    folders = ['_indicators', '_goals', '_pages']
     for folder in folders:
         new_folder = os.path.join(folder, new_language)
         # Abort if folder already exists.
@@ -39,9 +39,16 @@ def add_language(new_language, language_public):
             shutil.copyfile(source_file, new_file)
         # Change the copied files to reflect the new language.
         for line in fileinput.input(glob.glob(new_folder + '/*'), inplace=True):
+            # Skip a few unnecessary lines.
+            if folder == '_goals' and line.startswith('title:'):
+                continue
+            if folder == '_goals' and line.startswith('short:'):
+                continue
+            if folder == '_goals' and line.startswith(' '):
+                continue
             # Look for the permalink line, and add the language.
             if line.startswith('permalink: /'):
-                sys.stdout.write(line.replace('permalink: /', 'permalink: /' + language_public + '/'))
+                sys.stdout.write(line.replace('permalink: /', 'permalink: /' + new_language + '/'))
                 sys.stdout.write('language: ' + new_language + '\n')
             else:
                 sys.stdout.write(line)
@@ -50,12 +57,9 @@ def main():
 
     # Abort if there is no parameter provided.
     if len(sys.argv) < 2:
-        sys.exit('Provide a 2-letter abbreviation for the new language. Optionally also provide a second alternative abbreviation to use in permalinks.')
+        sys.exit('Provide a 2-letter abbreviation for the new language.')
     language = sys.argv[1]
-    language_public = language
-    if len(sys.argv) == 3:
-        language_public = sys.argv[2]
-    add_language(language, language_public)
+    add_language(language)
     print("Don't forget to update the 'languages' list in _config.yml!")
 
 # Boilerplace syntax for running the main function.
